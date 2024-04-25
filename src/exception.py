1b2c8276c9e88b791dda4f8b684fa014d80001a3
Soumya Mittal <soumyamittalrgh@gmail.com>
2024-04-25 17:34:45 +0530

    Data Transformation

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
-    _,_,error_detail.exc_info()
+    _,_error_detail.exc_info()
     file_name=exc_tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
         file_name,exc_tb.tb_lineno,str(error))
